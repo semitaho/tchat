cc7ce7ce31a1b3ae466981cb5d4ae5afd4826139
Toni Aho <toni.aho@siili.fi>
2014-10-22 23:28:11 +0300

    mongo

--- src/js/loginController.js
+++ src/js/loginController.js
@@ -1,8 +1,9 @@
-angular.module('tchat-app').controller('LoginController', ['$scope', 'Auth', function($scope, Auth){
+angular.module('tchat-app').controller('LoginController', ['$scope', '$state', 'Auth', function($scope, $state,Auth){
 	$scope.login = function(){
 		console.log('doing login...');
 
 		Auth.login($scope.nick);
+    $state.go('main');
 	}
 
 
